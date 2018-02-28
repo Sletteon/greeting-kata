ccae4d677aeb2dad99bbda568179986bf112ce77
sletteon <misi.g126@gmail.com>
2018-02-28 10:25:31 +0100

    holidaygreeting, and with frames

--- greet.py
+++ greet.py
@@ -1,9 +1,22 @@
 #!usr/env python3
 
+def goodmorning(name):
+    print('Good mornin, {}.'.format(name))
+
+def holidayGreeting(name, holiday, appropiateGreeting):
+    return '{} {}, {}!'.format(appropiateGreeting, holiday, name)
+
+def fancyFrame(functionOutput):
+    print('*********************************************************************************')
+    print(functionOutput)
+    print('*********************************************************************************')
+
 def Main():
-    print("Good mornin")
+
+    holidayGreetingParams = ('misi', 'Eastern', 'Happy')
+    fancyFrame(holidayGreeting(*holidayGreetingParams))
+    
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     Main()
